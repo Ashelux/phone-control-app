41e43a278ee02028b058f57e8386924273aa9c19
ashe <285483831@qq.com>
2026-04-19 15:07:35 +0800

    Remove Shizuku dependency, use Termux Intent instead

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,9 +49,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("com.google.code.gson:gson:2.10.1")
 
-    // Shizuku API
-    implementation("dev.rikka.shizuku:api:13")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
